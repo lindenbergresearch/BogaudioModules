37b5bbf242ea50318cfd590af3c3c4ee0ae0d38e
Patrick Lindenberg <heapdump@icloud.com>
2021-08-03 17:08:30 +0200

    add helper method for converting a scalar value to a RGB color redefine config params reduce to 4 channels

--- src/AnalyzerXL.hpp
+++ src/AnalyzerXL.hpp
@@ -9,6 +9,10 @@ namespace bogaudio {
 
 struct AnalyzerXL : AnalyzerBase {
     enum ParamsIds {
+        CONTROLA_PARAM,
+        CONTROLB_PARAM,
+        CONTROLC_PARAM,
+        CONTROLD_PARAM,
         NUM_PARAMS
     };
 
@@ -17,10 +21,6 @@ struct AnalyzerXL : AnalyzerBase {
         SIGNALB_INPUT,
         SIGNALC_INPUT,
         SIGNALD_INPUT,
-        SIGNALE_INPUT,
-        SIGNALF_INPUT,
-        SIGNALG_INPUT,
-        SIGNALH_INPUT,
         NUM_INPUTS
     };
 
@@ -36,6 +36,29 @@ struct AnalyzerXL : AnalyzerBase {
 
     AnalyzerXL() : AnalyzerBase(8, NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {
         _skinnable = false;
+        configParam(CONTROLA_PARAM, 0.0f, 1.0f, 1.0f, "CTRL A");
+        configParam(CONTROLB_PARAM, 0.0f, 1.0f, 1.0f, "CTRL B");
+        configParam(CONTROLC_PARAM, 0.0f, 1.0f, 1.0f, "CTRL C");
+        configParam(CONTROLD_PARAM, 0.0f, 1.0f, 1.0f, "CTRL D");
+    }
+
+
+    /**
+     * @brief Converts a linear, normalised, scalar value to a RGB encoded 3-Byte color.
+     * @param x 0.f .. 1.f
+     * @return NVGcolor
+     */
+    static inline NVGcolor scalar2color(float x, float alpha) {
+        // max colors with 3 byte resolution
+        constexpr int MAX_COLOR_VAL = 256 * 256 * 256;
+
+        union {
+            unsigned char rgb[3] = { 0, 0, 0 };
+            unsigned int color;
+        };
+
+        color = uint(x * MAX_COLOR_VAL);
+        return nvgRGBA(rgb[0], rgb[1], rgb[2], alpha * 255);
     }
 
 
