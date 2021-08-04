f332412aabb449df3f2cefdb74b6d8fe4ff5c598
Patrick Lindenberg <heapdump@icloud.com>
2021-08-04 11:13:48 +0200

    remove unused

--- src/bogaudio.cpp
+++ src/bogaudio.cpp
@@ -10,25 +10,8 @@ Plugin *pluginInstance;
 
 void init(rack::Plugin *p) {
     pluginInstance = p;
+
     p->addModel(modelAnalyzer);
     p->addModel(modelAnalyzerXL);
     p->addModel(modelRanalyzer);
-
-
-#ifdef EXPERIMENTAL
-    p->addModel(modelLag);
-    p->addModel(modelPEQ14XR);
-    p->addModel(modelPEQ14XV);
-#endif
-
-#ifdef TEST
-    p->addModel(modelTest);
-    p->addModel(modelTest2);
-    p->addModel(modelTestExpanderBase);
-    p->addModel(modelTestExpanderExtension);
-    p->addModel(modelTestGl);
-    p->addModel(modelTestVCF);
-#endif
-
-    //NEW_MODELS_HERE
 }
