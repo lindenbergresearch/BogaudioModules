bc3d0ecc6388b97d3b9a3fdd6324639347ccb2b3
Patrick Lindenberg <heapdump@icloud.com>
2023-01-11 21:25:56 +0100

    clean file

--- src/bogaudio.cpp
+++ src/bogaudio.cpp
@@ -1,17 +1,11 @@
 
 #include "bogaudio.hpp"
-
-#include "Analyzer.hpp"
 #include "AnalyzerXL.hpp"
-#include "Ranalyzer.hpp"
 
 Plugin *pluginInstance;
 
 
 void init(rack::Plugin *p) {
     pluginInstance = p;
-
-    p->addModel(modelAnalyzer);
     p->addModel(modelAnalyzerXL);
-    p->addModel(modelRanalyzer);
 }
