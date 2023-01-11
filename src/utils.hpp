5e16f05ac0e9023dbe23ef44a413f26ba1ed7aa1
Patrick Lindenberg <heapdump@icloud.com>
2023-01-11 21:25:34 +0100

    update to new model handling

--- src/utils.hpp
+++ src/utils.hpp
@@ -9,13 +9,8 @@ using namespace rack;
 
 namespace bogaudio {
 
-template<typename TModule, typename TModuleWidget, typename... Tags>
-Model *createModel(
-      const char *slug,
-      const char *name,
-      const char *description,
-      Tags... tags
-) {
+template<typename TModule, typename TModuleWidget>
+Model *createModel(const char *slug) {
     // params besides slug are obsolete here, but see scripts/modules_to_plugin_json.rb
     return rack::createModel<TModule, TModuleWidget>(slug);
 }
